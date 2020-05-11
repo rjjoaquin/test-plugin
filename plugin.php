6ad9fe3f768133afa9919f9abcca454c494ac68a
rjjoaquin <46547515+rjjoaquin@users.noreply.github.com>
2020-05-11 18:32:35 +0800

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -8,11 +8,11 @@
 
 
 function the_table ( $content ) {
-    return $content .= <table>
+    return $content .= `<table>
     <tr><th>Column 1</th><th>Column 2</th<</tr>
     <tr><td>Column 1 Text 1</td><td>Column 2 Text 1</td></tr>
     <tr><td>Column 1 Text 2</td><td>Column 2 Text 2</td></tr>
-    </table>;
+    </table>`;
 }
 
 add_action( 'the_content', 'the_table' );
